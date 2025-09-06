1225d856fb20c1c9a64e59ab83476b02e1326acd
Cesar-ES1 <hunsion.hornet@gmail.com>
2025-09-06 17:52:54 -0500

    Primer commit

---
+++ src/main/java/com/example/back_vallespejo/BackVallespejoApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_vallespejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackVallespejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackVallespejoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/back_vallespejo/BackVallespejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_vallespejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackVallespejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
