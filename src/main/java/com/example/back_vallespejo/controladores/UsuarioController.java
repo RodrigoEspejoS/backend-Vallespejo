276c3c625772c62aaecf558587935238824426f6
RodrigoEspejoS <u21316774@utp.edu.pe>
2025-09-24 23:21:48 -0500

    Configuración de Cors

--- src/main/java/com/example/back_vallespejo/controladores/UsuarioController.java
+++ src/main/java/com/example/back_vallespejo/controladores/UsuarioController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
 @CrossOrigin
+@RequestMapping("/api")
 public class UsuarioController {
 
     @Autowired
